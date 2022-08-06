29133d852380efcdc830616632e26f4db21fa981
faheemlegend <110661950+faheemlegend@users.noreply.github.com>
2022-08-06 11:47:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
+var host = data["Microseason2.aternos.me"];
 var username = data["name"]
 var bot = mineflayer.createBot({
   host: host,
